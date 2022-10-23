117a1f8c0d55b2954056436aa1b2502ab3d2331e
Michal <michalgrzegorczyk04@gmail.com>
2022-10-23 14:47:55 +0200

    Styles group

--- src/app/core/table.animations.ts
+++ src/app/core/table.animations.ts
@@ -1,14 +1,41 @@
-import { trigger, animate, style, state, transition } from '@angular/animations';
+import { trigger, animate, style, state, transition, group } from '@angular/animations';
 
+const commonStyles = {
+  border: '4px solid black',
+  color: 'white'
+}
 export const HighlightTrigger = trigger('rowHighlight', [
-  state('selected', style({
+  state('selected', style([commonStyles, {
     backgroundColor: 'lightgreen',
     fontSize: '20px'
-  })),
-  state('notselected', style({
+  }])),
+  state('notselected', style([commonStyles, {
     backgroundColor: 'lightsalmon',
-    fontSize: '22px'
+    fontSize: '22px',
+    color: 'black'
+  }])),
+  state('void', style({
+    opacity: 0
   })),
-  transition('selected => notselected', animate('200ms')),
-  transition('notselected => selected', animate('200ms'))
+  transition('* => notselected', animate('200ms')),
+  transition('* => selected',
+    [
+      animate(
+        '400ms 200ms ease-in',
+        style({
+          backgroundColor: 'lightblue',
+          fontSize: '25px'
+        })),
+        group([
+          animate('250ms', style({
+            backgroundColor: 'lightcoral',
+          })),
+          animate('450ms', style({
+            fontSize: '30px'
+          }))
+        ]),
+      animate('200ms')
+    ]
+  ),
+  transition('void => *', animate('500ms'))
 ]);
