213264056be83f04cb0ac5a559de84761e3deddd
Michal <michalgrzegorczyk04@gmail.com>
2022-10-23 15:01:52 +0200

    Use framework styles

--- src/app/core/table.animations.ts
+++ src/app/core/table.animations.ts
@@ -1,21 +1,11 @@
 import { trigger, animate, style, state, transition, group } from '@angular/animations';
+import { getStylesFromClasses } from './animationUtils';
 
-const commonStyles = {
-  border: '4px solid black',
-  color: 'white'
-}
 export const HighlightTrigger = trigger('rowHighlight', [
-  state('selected', style([commonStyles, {
-    backgroundColor: 'lightgreen',
-    fontSize: '20px'
-  }])),
-  state('notselected', style([commonStyles, {
-    backgroundColor: 'lightsalmon',
-    fontSize: '22px',
-    color: 'black'
-  }])),
+  state('selected', style(getStylesFromClasses(['bg-success', 'h4']))),
+  state('notselected', style(getStylesFromClasses(['bg-info']))),
   state('void', style({
-    opacity: 0
+    transform: 'translateX(-50%)'
   })),
   transition('* => notselected', animate('200ms')),
   transition('* => selected',
@@ -24,16 +14,9 @@ export const HighlightTrigger = trigger('rowHighlight', [
         '400ms 200ms ease-in',
         style({
           backgroundColor: 'lightblue',
-          fontSize: '25px'
-        })),
-        group([
-          animate('250ms', style({
-            backgroundColor: 'lightcoral',
-          })),
-          animate('450ms', style({
-            fontSize: '30px'
-          }))
-        ]),
+          fontSize: '20px'
+        })
+      ),
       animate('200ms')
     ]
   ),
